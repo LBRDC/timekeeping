fb66349ae637ea2226423a71859c48f1f65d0ceb
Zhnnyyy <perpetuaedemmanuel1225@gmail.com>
2025-02-13 19:55:28 +0800

    Feb 13

--- admin/query/mobile/standalone/allfaces.php
+++ admin/query/mobile/standalone/allfaces.php
@@ -7,7 +7,7 @@ try {
     $json = file_get_contents('php://input');
     $data = json_decode($json, true);
     $location = $data['location'];
-    $query = "select face from mobile_account as ma where !ISNULL(ma.face) and ma.Location = :location";
+    $query = "select ma.face, ma.Employee as idnumber, concat(em.firstname, ' ', em.lastname) as name  from mobile_account as ma inner join employees as em on em.idnumber = ma.Employee where !ISNULL(ma.face) and ma.Location = :location";
     $stmt = $conn->prepare($query);
     $stmt->bindParam(':location', $location);
     $stmt->execute();
@@ -15,7 +15,7 @@ try {
     $data = [];
     foreach ($result as $row) {
         $face = $row['face'];
-        array_push($data, array("face" => $face));
+        array_push($data, array("face" => $face, "name" => $row['name'], "idnumber" => $row['idnumber']));
     }
     $response['msg'] = $data;
 } catch (Exception $e) {
